ccac295aa671425d1ff0c3675dd21eca23999b1b
Tyson Zimmer <zimmertyson@gmail.com>
2025-09-28 10:32:59 -0700

    added functions and parameters folder

---
+++ functions-and-parameters/script.js
@@ -0,0 +1,91 @@
+// Task 1: Create a function called greet
+// Task 2: Inside the function, console.log a greeting message
+// Task 3: Call the function below
+
+function greet() {
+  console.log("Hello Will and Colby");
+}
+
+greet();
+
+// --- 10 Practice Tasks ---
+// Example 1: Create a function that logs 'Hello!'
+
+function sayHello() {
+  console.log("Hello!");
+}
+
+sayHello();
+
+// Example 2: Create a function that logs 'Goodbye!'
+
+function sayGoodbye() {
+  console.log("Goodbye!");
+}
+
+sayGoodbye();
+
+// Example 3: Create a function that logs 'Welcome aboard!'
+
+function welcome() {
+  console.log("Welcome Aboard!");
+}
+
+welcome();
+// Example 4: Create a function that logs 'Thanks a lot!'
+
+function thankYou() {
+  console.log("Thanks a lot!");
+}
+
+thankYou();
+
+
+// Example 5: Create a function that logs 'You can do it!'
+
+function motivate() {
+  console.log("You can do it!");
+}
+
+motivate();
+
+
+// Example 6: Create a function that logs 'Today is a big day!'
+
+function bigDay() {
+  console.log("Today is a big day!");
+}
+
+bigDay();
+
+// Example 7: Create a function that logs 'An error occurred.'
+
+function errorMessage() {
+  console.warn("An error has occurred");
+}
+
+errorMessage();
+
+// Example 8: Create a function that logs 'Notification sent.'
+
+function notificationSent() {
+  console.log("Notification Sent");
+}
+
+notificationSent();
+
+// Example 9: Create a function that logs 'Alert: Action needed.'
+
+function actionNeeded() {
+  console.log("Alert: Action needed");
+}
+
+actionNeeded();
+
+// Example 10: Create a function that logs 'All done!'
+
+function allDone() {
+  console.log("All done!");
+}
+
+allDone();
